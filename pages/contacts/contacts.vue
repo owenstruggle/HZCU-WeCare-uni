da91ddb7d1ed3f49040d4603640eeb26efb223a7
OwemShu <shuhenxin@qq.com>
2022-11-29 15:42:37 +0800

    init project

---
+++ pages/contacts/contacts.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    Contacts
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
